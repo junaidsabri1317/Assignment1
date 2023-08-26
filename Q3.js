8ac84b3da51bc3c6ab854939d377abc00dd61cac
junaidsabri1317 <xhan.junaid@gmail.com>
2023-08-26 11:18:39 -0700

    first commit

--- Q3.js
+++ Q3.js
@@ -1,4 +1,16 @@
 "use strict";
-// Store your name in a variable and print it.
-var Name = "Junaid Sabri";
-console.log("My Name is", Name);
+// // // Store your name in a variable and print it.
+// // var Name:string = "Junaid Sabri"
+// // console.log("My Name is",Name);
+// Store 10 numbers in different variables.
+var num1 = 10;
+var num2 = 20;
+var num3 = 30;
+var num4 = 40;
+var num5 = 50;
+var num6 = 60;
+var num7 = 70;
+var num8 = 80;
+var num9 = 90;
+var num10 = 100;
+console.log(num1, num2, num3, num4, num5, num6, num7, num8, num9, num10);
--- Q3.ts
+++ Q3.ts
@@ -1,3 +1,17 @@
-// Store your name in a variable and print it.
-var Name:string = "Junaid Sabri"
-console.log("My Name is",Name);
+// // // Store your name in a variable and print it.
+// // var Name:string = "Junaid Sabri"
+// // console.log("My Name is",Name);
+
+
+// Store 10 numbers in different variables.
+var num1:number = 10
+var num2:number = 20
+var num3:number = 30
+var num4:number = 40
+var num5:number = 50
+var num6:number = 60
+var num7:number = 70
+var num8:number = 80
+var num9:number = 90
+var num10:number = 100
+console.log(num1,num2,num3,num4,num5,num6,num7,num8,num9,num10);
