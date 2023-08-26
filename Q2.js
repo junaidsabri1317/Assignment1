90a7d9d943b96366d39328afc2e0799d1fa4661f
junaidsabri1317 <xhan.junaid@gmail.com>
2023-08-26 09:12:21 -0700

    first commit

--- Q2.js
+++ Q2.js
@@ -1,3 +1,5 @@
 "use strict";
-var message = "Adnan Khalil";
+var message = "Junaid Sabri";
 console.log(message);
+var work = "My first work on git hub";
+console.log(message, work);
--- Q2.ts
+++ Q2.ts
@@ -1,2 +1,4 @@
 var message = "Junaid Sabri";
 console.log(message);
+var work = "My first work on git hub"
+console.log(message,work)
