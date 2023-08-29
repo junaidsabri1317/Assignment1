c19018487d13771a1f53669a2fa0a9c68ed8c7dc
junaidsabri1317 <xhan.junaid@gmail.com>
2023-08-29 11:07:52 -0700

    first commit

---
+++ q7.js
@@ -0,0 +1,18 @@
+"use strict";
+// Now, perform all four operations with the given numbers, print the results, and observe the order in which the operations take place.
+var num1 = 10;
+var num2 = 15;
+var result = num1 + num2;
+console.log(result);
+var abc = 2;
+var result1 = abc * result;
+console.log(result1);
+var num5 = 5;
+var result2 = result1 / num5;
+console.log(result2);
+var num6 = 5;
+var result3 = result2 - num6;
+console.log(result3);
+var num7 = 2;
+var result4 = result * result1 / result3 - num7;
+console.log(result4);
