f13589e7858891e5631b08633aa1d45deaccd187
junaidsabri1317 <xhan.junaid@gmail.com>
2023-08-29 10:42:55 -0700

    first commit

---
+++ q6.js
@@ -0,0 +1,7 @@
+"use strict";
+// Take two numbers and print the division result.
+var num1 = 100;
+var num2 = 15;
+var num3 = "junaid sabri";
+var result = num1 / num2;
+console.log("division", result);
---
+++ q6.ts
@@ -0,0 +1,6 @@
+// Take two numbers and print the division result.
+var num1:number = 100;
+var num2:number =15;
+var num3:string = "junaid sabri"
+var result:number = num1 / num2 
+console.log("division",result);
