218de0f56c9e89bf182151bd150a711785b30811
junaidsabri1317 <xhan.junaid@gmail.com>
2023-08-29 07:36:16 -0700

    first commit

---
+++ q5.ts
@@ -0,0 +1,14 @@
+// Print the Difference (subtraction).
+
+var numA: number = 1;
+var numB: number = 2;
+var numC: number = 4;
+var numD: number = 6;
+var numE: number = 8;
+var numF: number = 10;
+var numG: number = 12;
+var numH: number = 14;
+var numI: number = 16;
+var numJ: number = 18;
+var result: number = (numA-numB-numC-numD-numE-numF-numG-numH-numI-numJ);
+console.log("subtractionl",result);
