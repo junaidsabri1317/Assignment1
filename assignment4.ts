c1beb03ca6575b3c432612757710e9e16296a0d5
junaidsabri1317 <xhan.junaid@gmail.com>
2023-09-01 12:06:44 -0700

    assignment 4 added

---
+++ assignment4.ts
@@ -0,0 +1,109 @@
+// Q No.1.	Create a function that takes an array, an index, 
+// and a value as parameters. Inside the function, use the 
+// splice method to insert the value at the specified index in 
+// the array. Return the modified array.
+
+let studeNames1:string[]=["Adnan","Asif","Ghani","Mani"]
+function takeValueInarray(index1:number,index2:number,studentName:string)
+{
+    studeNames1.splice(index1,index2,studentName)
+    return studeNames1}
+var newArray=takeValueInarray(2,0,"Sabri")
+console.log(newArray)
+
+// Q No.2.	Implement a simple shopping cart program using an array. 
+// Create functions to add items, remove items,and update quantities
+//  using the splice method. Print the cart's contents after each operation
+var shoppingCart:string[]=["Egg","Bread","Milk","Jam", "Biscuits"]
+function changeInCart(index1:number,index2:number,item:string) 
+{
+    shoppingCart.splice(index1,index2,item)
+    return shoppingCart
+}
+var updatedCart=changeInCart(shoppingCart.length,0,"Baqarkhani")
+console.log(updatedCart)
+console.log("-----------------------------------------------------------")
+updatedCart.splice(updatedCart.length-1,1)
+console.log(updatedCart)
+console.log("-----------------------------------------------------------")
+
+
+
+// Q No.3.	Write a program that uses a while loop 
+// to print the first 25 integers.
+let integer:number=1
+while (integer<=25)
+{
+    console.log(integer)
+    integer++    
+}
+
+
+
+//Q No.4.	Write a program that uses a while loop to 
+// print the first 10 even numbers. 
+
+let numbers = 1;
+let evenNumbers = 0;
+while (evenNumbers < 10) 
+{
+  if (numbers % 2 === 0) 
+  {
+    console.log(numbers);
+    evenNumbers++;
+  }
+  numbers++;
+}
+
+
+//Q No.5.	Create a function that takes a positive integer as 
+//parameter and uses a while loop to calculate and return the 
+//factorial of that number.
+
+function factorial(num:number) 
+{
+    var num1:number=1
+    while (num>0) 
+    {
+        num1 = num1*num
+        num--
+    }
+    return num1;
+}
+var newFactorial:number=factorial(5)
+console.log(newFactorial)
+
+
+
+//Q No.8.	Implement a program that takes a list of temperatures in Celsius as input 
+//from the user. Convert each temperature to Fahrenheit using the formula 
+//F = (C * 9/5) + 32 and store the converted temperatures in an array. 
+//Use a while loop to perform the conversion for each temperature.
+var tempInCels: number[] = [40, 0, -10, 15];
+var tempInFh: number[] = convertCelsToFh(tempInCels);
+
+function convertToFahrenheit(celsius: number): number 
+{
+  return (celsius * 9/5) + 32;
+}
+
+function convertCelsToFh(celsiusTemperatures: number[]): number[] 
+{
+  var fahrenheitTemperatures: number[] = [];
+  
+  let index = 0;
+  while (index < celsiusTemperatures.length) 
+  {
+    var celsius = celsiusTemperatures[index];
+    var fahrenheit = convertToFahrenheit(celsius);
+    fahrenheitTemperatures.push(fahrenheit);
+    index++;
+  }
+  return fahrenheitTemperatures;
+}
+console.log("--------------------------------------------------------------------");
+console.log("Temperatures in Celsius   :",tempInCels);
+console.log("--------------------------------------------------------------------");
+console.log("Temperatures in Fahrenheit:", tempInFh);
+console.log("--------------------------------------------------------------------");
+
